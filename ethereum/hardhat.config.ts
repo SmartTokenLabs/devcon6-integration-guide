0369fa1aa53bc8a843eae210428c3771e5baa452
Michael Wallace <micwallace@gmx.com>
2022-09-26 14:40:39 +1000

    Update versions

--- ethereum/hardhat.config.ts
+++ ethereum/hardhat.config.ts
@@ -5,7 +5,10 @@ const config: HardhatUserConfig = {
   solidity: "0.8.17",
 	networks: {
 	  hardhat: {
-		  allowUnlimitedContractSize: true
+		  allowUnlimitedContractSize: true,
+		  forking: {
+			  url: "https://eth-goerli.alchemyapi.io/v2/9jRnyHMPq41aZ-TXD4jIXEkZFBUa4J0r"
+		  }
 	  }
 	}
 };
